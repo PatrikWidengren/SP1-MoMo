5a33bfaa78748ed456c3eaed7cd36188d189a217
kakaopirat <a15simhe@student.his.se>
2016-03-07 15:04:30 +0100

    SmallFix

--- Spelprojekt/GameStates/Menu/WinMenu.cpp
+++ Spelprojekt/GameStates/Menu/WinMenu.cpp
@@ -2,28 +2,11 @@
 
 WinMenu::WinMenu(float width, float height)
 {
-	//highlightSprite01.setPosition(10, 235);
 	mTimer = 0;
 	mState = 6;
 	setFonts();
 	setTextures();
-	/*
 
-	menu[0].setFont(font);
-	menu[0].setColor(sf::Color::Red);
-	menu[0].setString("New Game");
-	menu[0].setPosition(sf::Vector2f(width / 2, height / (3 + 1) * 2));
-
-	menu[1].setFont(font);
-	menu[1].setColor(sf::Color::White);
-	menu[1].setString("Exit");
-	menu[1].setPosition(sf::Vector2f(width / 2, height / (3 + 1) * 3));
-
-	mCompleteText.setFont(font);
-	mCompleteText.setColor(sf::Color::Cyan);
-	mCompleteText.setString("Stage Complete!");
-	mCompleteText.setPosition(sf::Vector2f(width / 2, height / (3 + 1) * 1));
-	*/
 	selectedIndex = 0;
 
 	mRects[0] = new sf::IntRect(sf::Vector2i(500, 640), sf::Vector2i(550, 100));
@@ -79,7 +62,7 @@ void WinMenu::updateWinMenu(sf::RenderWindow &window, sf::Vector2i &mouse)
 		mReturn = false;
 		if (selectedIndex == 0) {
 			reset = true;
-			mState = 1;
+			mState = 3;
 		}
 		if (selectedIndex == 1) {
 			window.close();
@@ -89,29 +72,13 @@ void WinMenu::updateWinMenu(sf::RenderWindow &window, sf::Vector2i &mouse)
 
 void WinMenu::displayMenu01(sf::RenderWindow &window)
 {
-	/*
-	for (int i = 0; i < mNumberOfSelections; i++)
-	{
-		window.draw(menu[i]);
-	}
-	*/
 
-	//window.draw(mCompleteText);
 	window.draw(bg01);
-	//std::cout << mMouse.x << ": 1 :" << mMouse.y << std::endl;
-	//std::cout << highlightSprite01.getPosition().x << ": WinMenu :" << highlightSprite01.getPosition().y << std::endl;
 
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && mMouse.x > 10 && mMouse.x < 200 && mMouse.y > 235 && mMouse.y < 365) // left click if its on the option
 	{
 		window.close();
 	}
-	/*if (mMouse.x > highlightSprite01.getPosition().x * highlightSprite01.getScale().x && mMouse.x < highlightSprite01.getPosition().y - 35 && mMouse.y > highlightSprite01.getPosition().y && mMouse.y < highlightSprite01.getPosition().y + 30)
-	{
-		highlightSprite01.setPosition(10, 235);
-		window.draw(highlightSprite01);
-	}*/
-
-
 }
 
 void WinMenu::setTextures()
@@ -121,8 +88,6 @@ void WinMenu::setTextures()
 	textBg01.loadFromFile("error.jpg");
 
 	bg01.setTexture(textBg01);
-	//highlightSprite01.setTexture(highlighttextBg01);
-
 }
 
 void WinMenu::setFonts()
