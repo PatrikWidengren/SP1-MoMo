f5f7f769a93795af6e4ffd564b8fc5f6ee440020
PatrikWidengren <a15patwi@student.his.se>
2016-03-08 11:37:13 +0100

    Some fixes

--- Spelprojekt/GameStates/RegionMap.cpp
+++ Spelprojekt/GameStates/RegionMap.cpp
@@ -82,6 +82,7 @@ void RegionMap::updateRegionMap(sf::RenderWindow &window, sf::Vector2i &mouse)
 
 		spriteGrassMower.setPosition(140 * bg01.getScale().x, 1120 * bg01.getScale().y);
 		spriteHedgecutter.setPosition(630 * bg01.getScale().x, 1120 * bg01.getScale().y);
+		
 		mRects[3] = new sf::IntRect(sf::Vector2i(3 * bg01.getScale().x, 11200 * bg01.getScale().y), sf::Vector2i(116 * bg01.getScale().x, 173 * bg01.getScale().y));
 		mRects[4] = new sf::IntRect(sf::Vector2i(285 * bg01.getScale().x, 11200 * bg01.getScale().y), sf::Vector2i(116 * bg01.getScale().x, 180 * bg01.getScale().y));
 		mRects[5] = new sf::IntRect(sf::Vector2i(493 * bg01.getScale().x, 11200 * bg01.getScale().y), sf::Vector2i(116 * bg01.getScale().x, 173 * bg01.getScale().y));
@@ -102,7 +103,6 @@ void RegionMap::updateRegionMap(sf::RenderWindow &window, sf::Vector2i &mouse)
 
 			spriteGrassMower.move(0 * bg01.getScale().x, 4 * bg01.getScale().y);
 			spriteHedgecutter.move(0 * bg01.getScale().x, 4 * bg01.getScale().y);
-
 
 			mRects[3] = new sf::IntRect(sf::Vector2i(3 * bg01.getScale().x, 11200 * bg01.getScale().y), sf::Vector2i(116 * bg01.getScale().x, 173 * bg01.getScale().y));
 			mRects[4] = new sf::IntRect(sf::Vector2i(287 * bg01.getScale().x, 11200 * bg01.getScale().y), sf::Vector2i(116 * bg01.getScale().x, 180 * bg01.getScale().y));
@@ -186,12 +186,14 @@ void RegionMap::updateRegionMap(sf::RenderWindow &window, sf::Vector2i &mouse)
 
 	}
 
-	for (int i = 0; i < mRegions[mRegionState].levelCount; i++) {
-		if (mLevelRects[i].contains(sf::Vector2i(mMouse.x, mMouse.y))) {
-			std::cout << "It's inside " << i << "!" << std::endl;
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && mClick) {
-				mLevelToLoad = i+1;
-				mState = 8;
+	if (mInternalState == 1) {
+		for (int i = 0; i < mRegions[mRegionState].levelCount; i++) {
+			if (mLevelRects[i].contains(sf::Vector2i(mMouse.x, mMouse.y))) {
+				std::cout << "It's inside " << i << "!" << std::endl;
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && mClick) {
+					mLevelToLoad = i + 1;
+					mState = 8;
+				}
 			}
 		}
 	}
@@ -338,8 +340,10 @@ void RegionMap::displayMenu01(sf::RenderWindow &window)
 	window.draw(spriteArrow04);
 	window.draw(spriteHedgecutter);
 	window.draw(spriteGrassMower);
-	for (int i = 0; i < mRegions[mRegionState].levelCount; i++) {
-		window.draw(spriteLevels[i]);
+	if (mInternalState == 1) {
+		for (int i = 0; i < mRegions[mRegionState].levelCount; i++) {
+			window.draw(spriteLevels[i]);
+		}
 	}
 
 	/*
@@ -452,10 +456,11 @@ void RegionMap::setRegionState(int state) {
 	spriteLevels = new sf::Sprite[mRegions[mRegionState].levelCount];
 	mLevelRects = new sf::IntRect[mRegions[mRegionState].levelCount];
 	for (int i = 0; i < mRegions[mRegionState].levelCount; i++) {
-		mLevelRects[i] = sf::IntRect(sf::Vector2i(0, i * 100), sf::Vector2i(200, 100));
+		mLevelRects[i] = sf::IntRect(sf::Vector2i(0 * bg01.getScale().x, i * 100 * bg01.getScale().y), sf::Vector2i(200 * bg01.getScale().x, 100 * bg01.getScale().y));
 	}
 	for (int i = 0; i < mRegions[mRegionState].levelCount; i++) {
-		spriteLevels[i].setPosition(0, i * 100);
+		spriteLevels[i].setPosition(0 * bg01.getScale().x, i * 100 * bg01.getScale().y);
+		spriteLevels[i].setScale(bg01.getScale().x, bg01.getScale().y);
 		spriteLevels[i].setTexture(textLevels);
 	}
 }
@@ -488,6 +493,11 @@ void RegionMap::scale(sf::RenderWindow &window) {
 	spriteArrow02.setScale((float)window.getSize().x / 1920, (float)window.getSize().y / 1080);
 	spriteArrow03.setScale(-(float)window.getSize().x / 1920, (float)window.getSize().y / 1080);
 	spriteArrow04.setScale((float)window.getSize().x / 1920, (float)window.getSize().y / 1080);
+
+	for (int i = 0; i < mRegions[mRegionState].levelCount; i++) {
+		spriteLevels[i].setScale((float)window.getSize().x / 1920, (float)window.getSize().y / 1080);
+	}
+
 
 	mRects[0] = new sf::IntRect(sf::Vector2i(360 * bg01.getScale().x, 295 * bg01.getScale().y), sf::Vector2i(410 * bg01.getScale().x, 200 * bg01.getScale().y));
 	mRects[1] = new sf::IntRect(sf::Vector2i(925 * bg01.getScale().x, 300 * bg01.getScale().y), sf::Vector2i(395 * bg01.getScale().x, 200 * bg01.getScale().y));
